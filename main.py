0cb619a4238e98b8f40b0c7e9946bda600cc5359
Mira <mira.theidel@gmx.de>
2026-01-17 21:34:06 +0100

    refactor: update get_sobject_list function to include headers and error handling

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import requests
 mcp = FastMCP("Test Data")
 
 @mcp.tool()
-def get_schema(domain_name: str) -> dict:
-    sObjects_url = f"https://{domain_name}.my.salesforce.com/services/data/v65.0/sobjects/"
-    response = requests.get(sObjects_url)
-    return response.json()
+def get_sobject_list(scratch_org_alias: str) -> dict:
+    """gets a list of sobjects from a specific scratch org"""
+    sObjects_url = f"https://{scratch_org_alias}.trailblaze.my.salesforce.com/services/data/v65.0/sobjects/"
+    headers = {'Content-Type': 'application/json'}
+    response = requests.get(sObjects_url, headers=headers)
+    sobjects=  response.json()
+    return sobjects if sobjects else {"error": "No sObjects found"}
 
 if __name__ == "__main__":
     mcp.run()
